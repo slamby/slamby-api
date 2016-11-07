43972c1a3f67d2b43a8b33a7fc9c8027be3c4165
Attila Laszlo <attila.laszlo@slamby.com>
2016-11-07 15:53:03 +0100

    modified build script

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -e
 dotnet restore
 rm -rf ./publish
 dotnet publish ./src/Slamby.API/project.json -c Release -o ./publish
-cp -r ./scripts publish
-chmod +x scripts/*.sh
+cp -r ./scripts ./publish/
+chmod +x ./publish/scripts/*.sh
