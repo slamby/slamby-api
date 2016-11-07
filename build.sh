f3377915fcd8ced192e5a8e13f31d7648f7cbdd4
Attila Laszlo <attila.laszlo@slamby.com>
2016-11-07 17:29:35 +0100

    add all appsettings to publishoptions

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 set -e
 dotnet restore
 rm -rf ./publish
-dotnet publish ./src/Slamby.API/project.json -c Release -o ./publish
+dotnet publish ./src/Slamby.API/project.json -c Release -o $(pwd)/publish
 cp -r ./scripts ./publish/
 chmod +x ./publish/scripts/*.sh
